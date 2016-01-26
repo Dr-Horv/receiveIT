e260d45adb42ef02c5dc237659c9a8ec5a4cc685
Christian Persson <saser@live.se>
2016-01-26 22:54:12 +0100

    Fix / route to return 404

--- src/main/scala/it/chalmers/recieveit/MainServlet.scala
+++ src/main/scala/it/chalmers/recieveit/MainServlet.scala
@@ -5,12 +5,7 @@ import org.scalatra._
 class MainServlet extends ScalatraServlet {
 
   get("/") {
-    Ok("Hello, World!")
-  }
-
-  get("/:name") {
-    val name = params("name")
-    Ok(s"Hello, $name!")
+    NotFound("")
   }
 
 }
