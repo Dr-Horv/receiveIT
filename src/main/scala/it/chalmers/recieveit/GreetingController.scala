9a562354b74ea73a77ebd621d60523d770bec7c0
Christian Persson <saser@live.se>
2016-01-26 22:49:21 +0100

    Update GreetingController to handle only /greeting endpoint

--- src/main/scala/it/chalmers/recieveit/GreetingController.scala
+++ src/main/scala/it/chalmers/recieveit/GreetingController.scala
@@ -4,6 +4,10 @@ import org.scalatra._
 
 class GreetingController extends ScalatraServlet {
 
+  get("/") {
+    Ok("Hello, World!")
+  }
+
   get("/:name") {
     val name = params("name")
     Ok(s"Hello, $name!")
