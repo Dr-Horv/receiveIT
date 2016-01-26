e5329a6fc78b8f70657ddc950470a9e8e136c735
Christian Persson <saser@live.se>
2016-01-26 22:44:59 +0100

    Remove /greeting test from MainServlet

--- src/test/scala/it/chalmers/recieveit/MainServletSpec.scala
+++ src/test/scala/it/chalmers/recieveit/MainServletSpec.scala
@@ -5,7 +5,6 @@ import org.scalatra.test.specs2._
 class MainServletSpec extends MutableScalatraSpec {
 
   addServlet(classOf[MainServlet], "/*")
-  addServlet(classOf[GreetingController], "/greeting/*")
 
   "GET on /" >> {
     "should return 200" >> {
@@ -21,17 +20,4 @@ class MainServletSpec extends MutableScalatraSpec {
     }
   }
 
-  "GET on /greeting/saser" >> {
-    "should return 200" >> {
-      get("/greeting/saser") {
-        status === 200
-      }
-    }
-
-    "should return \"Hello, saser!\"" >> {
-      get("/greeting/saser") {
-        response.body === "Hello, saser!"
-      }
-    }
-  }
 }
